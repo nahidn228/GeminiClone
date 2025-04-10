bbf26e9a94471a27033f3b237e44550c9ef0f971
Nahid Hasan <nahidn228@gmail.com>
2025-04-10 10:43:20 +0600

    complete UI

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,8 +5,7 @@ import { assets } from "../../assets/assets";
 const Main = () => {
   return (
     <div className="main">
-
-        {/* NavBar */}
+      {/* NavBar */}
       <div className="nav">
         <p>Nime</p>
         <img src={assets?.user_icon} alt="" />
@@ -14,32 +13,50 @@ const Main = () => {
 
       <div className="main-container">
         <div className="greet">
-            <p> <span>Hello, Dev..</span> </p>
-            <p>How can I help you today</p>
+          <p>
+            {" "}
+            <span>Hello, Dev..</span>{" "}
+          </p>
+          <p>How can I help you today</p>
         </div>
 
-      <div className="cards">
-        <div className="card">
+        {/* All cards */}
+        <div className="cards">
+          <div className="card">
             <p>Recommend a scenic destination for my next road trip.</p>
             <img src={assets?.compass_icon} alt="" />
-        </div>
-        <div className="card">
+          </div>
+          <div className="card">
             <p>What's a must-visit place for an adventurous road trip?</p>
             <img src={assets?.bulb_icon} alt="" />
-        </div>
-        <div className="card">
+          </div>
+          <div className="card">
             <p>Suggest a hidden gem to explore on a weekend drive.</p>
             <img src={assets?.message_icon} alt="" />
-        </div>
-        <div className="card">
+          </div>
+          <div className="card">
             <p>Improve the readability of the following code</p>
             <img src={assets?.code_icon} alt="" />
+          </div>
+        </div>
+
+        {/* Main bottom */}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a prompt here" />
+            <div>
+            <img src={assets?.gallery_icon} alt="" />
+            <img src={assets?.mic_icon} alt="" />
+            <img src={assets?.send_icon} alt="" />
+            </div>
+          </div>
+
+          <p className="bottom-info">
+          Nime may display inaccurate info, including about people, so double-check its responses. Your privacy and Nime Apps
+          </p>
         </div>
       </div>
-      </div>
-
-
-
     </div>
   );
 };
