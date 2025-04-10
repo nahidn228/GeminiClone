ff6855408a2232538e2da993fcd77dd29632957e
Nahid Hasan <nahidn228@gmail.com>
2025-04-10 09:12:33 +0600

    sidebar design done and added outfit font

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,40 +1,48 @@
+import { useState } from "react";
 import { assets } from "../../assets/assets";
 import "./Sidebar.css";
+
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
   return (
     <div className="sidebar">
-        {/* top section */}
+      {/* top section */}
       <div className="top">
-        <img className="menu" src={assets?.menu_icon} alt="" />
+        <img
+          onClick={() => setExtended(!extended)}
+          className="menu"
+          src={assets?.menu_icon}
+          alt=""
+        />
         <div className="new-chat">
           <img src={assets?.plus_icon} alt="" />
-          <p>New Chat</p>
+          {extended ? <p>New Chat</p> : null}
         </div>
-        <div className="recent">
-          <p className="recent-title">Recent</p>
-          <div className="recent-entry">
-            <img src={assets?.message_icon} alt="" />
-            <p>What is react ...</p>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets?.message_icon} alt="" />
+              <p>What is react ...</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
 
-
-       {/* Bottom section */}
+      {/* Bottom section */}
 
       <div className="bottom">
-
         <div className="bottom-item recent-entry">
-        <img src={assets?.question_icon} alt="" />
-        <p>Help </p>
+          <img src={assets?.question_icon} alt="" />
+          {extended ? <p>Help </p> : null}
         </div>
         <div className="bottom-item recent-entry">
-        <img src={assets?.history_icon} alt="" />
-        <p>Activity </p>
+          <img src={assets?.history_icon} alt="" />
+          {extended ? <p>Activity </p> : null}
         </div>
         <div className="bottom-item recent-entry">
-        <img src={assets?.setting_icon} alt="" />
-        <p>Settings </p>
+          <img src={assets?.setting_icon} alt="" />
+          {extended ? <p>Settings </p> : null}
         </div>
       </div>
     </div>
